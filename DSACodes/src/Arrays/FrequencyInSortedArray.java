552b48d754d8a87f132cf4d0586159e7ebeca833
deepdhar <dhar2017.slg@gmail.com>
2021-05-23 23:44:27 +0530

    updated DSA codes

--- DSACodes/src/Arrays/FrequencyInSortedArray.java
+++ DSACodes/src/Arrays/FrequencyInSortedArray.java
@@ -3,8 +3,9 @@ package Arrays;
 public class FrequencyInSortedArray {
 	
 	static void printFrequency(int arr[]) {
-		int freq=1, i=1, n=arr.length;
+		int i=1, n=arr.length;
 		while(i<n) {
+			int freq=1;
 			while(i<n && arr[i] == arr[i-1]) {
 				freq++; 
 				i++;
@@ -12,7 +13,6 @@ public class FrequencyInSortedArray {
 			
 			System.out.println(arr[i-1] + " " + freq);
 			i++;
-			freq=1;
 		}
 		if(n==1 || arr[n-1]!=arr[n-2])
 			System.out.println(arr[n-1] + " " + 1);
